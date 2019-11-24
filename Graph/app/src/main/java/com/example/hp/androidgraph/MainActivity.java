0abe9e53cde5c5aafcf32f9081d4f9cf01b584d2
Nalinswarup123 <nalinswarup@gmail.com>
2019-11-24 12:47:46 +0530

    Added point graph

--- Graph/app/src/main/java/com/example/hp/androidgraph/MainActivity.java
+++ Graph/app/src/main/java/com/example/hp/androidgraph/MainActivity.java
@@ -3,11 +3,26 @@ package com.example.hp.androidgraph;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+import com.jjoe64.graphview.series.PointsGraphSeries;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+        PointsGraphSeries<DataPoint> series = new PointsGraphSeries<>(new DataPoint[] {
+                new DataPoint(0, 1),
+                new DataPoint(1, 5),
+                new DataPoint(2, 3),
+                new DataPoint(3, 2),
+                new DataPoint(4, 6)
+        });
+        graph.addSeries(series);
     }
 }
